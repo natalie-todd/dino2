06f9e8218762256a780821066138b7c18c150d74
Natalie <natalie.todd15@gmail.com>
2018-07-24 12:19:05 -0600

    profiles done, skilllist recognized. w

--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import Skilllist from './Skilllist';
 
-export default function Profiles({ employee }) {
+export default function Profiles({ employeeinfo }) {
 
     return (
+
         <ul id='profiles'>
             {
-                employee.map(employee => (
-                    <li key={employee.id}>
-                        <img src={employee.image} />
-                        <h2>{employee.name}</h2>
+                employeeinfo.map((employee, index) => (
+                    <li key={index}>
+                        <div className='profile-card'>
+                            <header className='profile-header'>
+                                <img src={employee.image} />
+                                <h2>{employee.name}</h2>
+                                <Skilllist />
+                            </header>
+                        </div>
                     </li>
                 ))}
         </ul>
