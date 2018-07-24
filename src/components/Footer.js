0a325fa2c8fe6c65f6261355928b4ab890704e2c
Natalie <natalie.todd15@gmail.com>
2018-07-24 01:14:54 -0600

    footer done

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+        <footer>
+            <small>
+            © 2018
+            </small>
+        </footer>
+    )
+}
+
+export default Footer
